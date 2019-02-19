c9959c2f725180c314b1d036093e30daa0d2121b
Dan Yael Sajarópulos Verdugo <danyael031@gmail.com>
2019-02-19 08:45:24 -0700

    Práctica revisada

--- Microcontroladores/Ejercicio ADC/Ejercicio_ADC/Ejercicio_ADC.ino
+++ Microcontroladores/Ejercicio ADC/Ejercicio_ADC/Ejercicio_ADC.ino
@@ -16,11 +16,13 @@ int valorEntrada = 0;
 float voltaje = 0;
 float valorPorBit = 0.00488758553275;// valor por cada bit en un ADC de 10 bits con 5v de referencia
 
+
 long int tf = 0;
 
 hcsr04 ultrasonico;//declarando sensor ultrasónico
 
 cono elCono;
+cilindro elCilindro = {45, 15};
  
 void setup() {
 Serial.begin(9600);
@@ -52,11 +54,17 @@ void analogo(){
     valorEntrada = analogRead(entrada);
   voltaje = calcularVoltaje(valorEntrada);
   if (tf <= millis()){
+        unsigned int distancia = distanciaHCSR04(&ultrasonico);
+        
+        double volTot = volumenTotal(&elCono, &elCilindro, distancia);
         
         Serial.print("voltaje: ");
         Serial.println(voltaje);
         Serial.print("distancia: ");
-        Serial.println(distanciaHCSR04(&ultrasonico));
+        Serial.println(distancia);
+        Serial.print("volumen:");
+        Serial.println(volTot);
+        
 
       tf = millis() + 700;
     }
@@ -67,3 +75,24 @@ void analogo(){
     digitalWrite(led, HIGH);
   }
 }
+
+double volumenTotal(cono* conito, cilindro* cilindrito, unsigned int distancia){
+   double maxDist = conito->H + cilindrito->H;
+
+   double altRel = maxDist - (double) distancia;
+
+
+   if (distancia > maxDist){
+      //double volumen = volumenCilindro(cilindrito->R,cilindrito->H) + volumenConoTruncado(conito, 0.0);
+      return 0.0;
+    }
+    else if (altRel <= cilindrito->H){
+      double volumen = volumenCilindro(cilindrito->R, altRel);
+      return volumen;
+      }
+    else{
+      double volumen = volumenCilindro(cilindrito->R,cilindrito->H) + volumenConoTruncado(conito, (double)distancia);
+      return volumen;
+      }
+  
+  }
